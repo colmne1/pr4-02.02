4c6b097cbde45942a1bf9e42f683257d48418ae0
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-15 17:58:26 +0500

    Созданный класс FileInfoFTP.Kochetov

---
+++ Common/FileInfoFTP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class FileInfoFTP
+    {
+        public byte[] Data { get; set; }
+        public string Name { get; set; }
+        public FileInfoFTP(byte[] data, string name)
+        {
+            this.Data = data;
+            this.Name = name;
+        }
+    }
+}
