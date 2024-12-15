4c559e6da53d29304ff26cd7b6e1e3f3f24a7c8c
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-15 18:11:21 +0500

    Прописали метод GetDirectory.Kochetov

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.IO;
 
 namespace Server
 {
@@ -21,6 +22,26 @@ namespace Server
             User user = null;
             user = Users.Find(x => x.login == login && x.password == password);
             return user != null;
+        }
+        public static List<string> GetDirectory(string src)
+        {
+            List<string> FolderFiles = new List<string>();
+            if (Directory.Exists(src))
+            {
+                string[] dirs = Directory.GetDirectories(src);
+                foreach (string dir in dirs)
+                {
+                    string NameDirectory = dir.Replace(src, "");
+                    FolderFiles.Add(NameDirectory + "/");
+                }
+                string[] files = Directory.GetFiles(src);
+                foreach (string file in files)
+                {
+                    string NameFile = file.Replace(src, "");
+                    FolderFiles.Add(NameFile);
+                }
+            }
+            return FolderFiles;
         }
     }
 }
