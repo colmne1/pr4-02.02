9e1177c71429bd3bd350e7eef00b68230974499c
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-15 18:02:49 +0500

    Созданный класс User и созданны переменные в проекте Server. Kochetiv

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    public class Program
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IAddress;
+        public static int Port;
         static void Main(string[] args)
         {
         }
