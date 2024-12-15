10c0489bf1ed93d2a3eb3e7ca597df8b91c3fc68
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-15 18:03:49 +0500

    Прописанный метод AutorizationUser.Kochetov

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace Server
 {
@@ -14,6 +15,12 @@ namespace Server
         public static int Port;
         static void Main(string[] args)
         {
+        }
+        public static bool AutorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+            return user != null;
         }
     }
 }
