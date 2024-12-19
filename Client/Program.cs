264094557dce4713e7b002d53f41a6a147760cc3
Shizyn412 <juetjuifjhhsiehykeh@mail.ru>
2024-12-19 21:11:04 +0500

    прописал переменные. Осипов

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace Client
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        public static IPAddress IPAddress = IPAddress.Parse("127.0.0.1");
+        public static int Port = 5000;
+        public static List<string> folders = new List<string>();
+        public static int Id = -1;
     }
 }
