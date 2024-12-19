5360fab3ef1f7dcc1a505b402a1322a75a35d14b
Shizyn412 <juetjuifjhhsiehykeh@mail.ru>
2024-12-19 21:39:12 +0500

    Добавлена проверка для вводимых команд. Осипов

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,11 +4,52 @@ using System.Net;
 
 namespace Client
 {
-    internal class Program
+    public class Program
     {
         public static IPAddress IPAddress = IPAddress.Parse("127.0.0.1");
         public static int Port = 5000;
         public static List<string> folders = new List<string>();
         public static int Id = -1;
+    }
+    public static bool CheckCommand(string message)
+    {
+        bool BCommand = false;
+        string[] DataMessage = message.Split(new string[1] { " " }, StringSplitOptions.None);
+        if (DataMessage.Length > 0)
+        {
+            string Command = DataMessage[0];
+            if (Command == "connect")
+            {
+                if (DataMessage.Length != 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Использование: connect [login] [password]\nПример: connect User1 Password");
+                    BCommand = false;
+                }
+                else BCommand = true;
+            }
+            else if (Command == "cd") BCommand = true;
+            else if (Command == "get")
+            {
+                if (DataMessage.Length == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Использование: get [NameFile]\nПример: get Test.txt");
+                    BCommand = false;
+                }
+                else BCommand = true;
+            }
+            else if (Command == "set")
+            {
+                if (DataMessage.Length == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Использование: set [NameFile]\nПример: set Test.txt");
+                    BCommand = false;
+                }
+                else BCommand = true;
+            }
+        }
+        return BCommand;
     }
 }
